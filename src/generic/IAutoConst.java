7046daa4475ed127dcea0b0d39dfd05d9a5097d0
javagalbhanu <javagalbhanu@gmail.com>
2017-12-24 20:55:16 -0800

    Initial commit

---
+++ src/generic/IAutoConst.java
@@ -0,0 +1,15 @@
+package generic;
+
+public interface IAutoConst {
+	String CHROME_KEY="webdriver.chrome.driver";
+	String CHROME_VALUE="./driver/chromedriver.exe";
+	String GECKO_KEY="webdriver.gecko.driver";
+	String GECK_VALUE="./driver/geckodriver.exe";
+	String XL_PATH="./data/input.xlsx";
+	String PHOTO_PATH="./photo/";
+	String SETTINGS_PATH="./settings.properties";
+}
+/**
+ * All the variables in interface are 
+ * public static final (constants)
+ */
